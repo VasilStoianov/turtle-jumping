dbeb203abe8729ddd20c695e009281f53c312d6d
lays <vaseto.stoianov.94@gmail.com>
2024-11-13 08:30:00 +0200

    Fixed bullets

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 const canvas = document.getElementById("game") as HTMLCanvasElement;
 const game = canvas.getContext("2d");
 
-const GAME_WIDTH = 1900 ;
+const GAME_WIDTH = 1900;
 const GAME_HEIGH = 720;
 let bullets: Bullet[] = [];
-
+let enemies: Enemy[] = [];
 enum States {
   RUN,
   JUMPSTART,
@@ -12,6 +12,60 @@ enum States {
   IDLE,
   FALL,
   ROLL,
+  HIT,
+  DEATH,
+}
+
+class Enemy {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  animations: Map<States, PlayerAnimation>;
+  scale: number = -1;
+  healtBar: {
+    width: number;
+    heidth: number;
+  };
+  speed: number = 6;
+  maxSpeed: number;
+  onGround: boolean = true;
+  vy: number = 1;
+  weigth: number = 0;
+  state: States;
+  health: number = 100;
+  isDead: boolean;
+  forRemoval: boolean = false;
+  constructor() {
+    this.isDead = false;
+    let rand = (Math.random() * (3 -1 + 1) + 1)
+    this.x = 450 * rand;
+    this.vy *= Math.floor(rand);
+    this.y = 255;
+    this.maxSpeed = 100;
+    this.width = 100;
+    this.height = 110;
+    this.state = States.RUN;
+
+    this.healtBar = {
+      width: 100,
+      heidth: 7,
+    };
+
+    this.animations = new Map<States, PlayerAnimation>();
+    this.animations.set(
+      States.RUN,
+      new PlayerAnimation(7, "assets/Enemies/Enemy 1/walk_")
+    );
+    this.animations.set(
+      States.HIT,
+      new PlayerAnimation(2, "assets/Enemies/Enemy 1/hit_")
+    );
+    this.animations.set(
+      States.DEATH,
+      new PlayerAnimation(9, "assets/Enemies/Enemy 1/death_")
+    );
+  }
 }
 
 class Bullet {
@@ -101,8 +155,12 @@ class Level {
 
   constructor(src: string) {
     this.blocks = [];
-    for (let x = 0; x <= 10; x++) {
-      let block = new Assets(src, {width:128, heidth:128},new Vector2D(x * 128, GAME_HEIGH - 128));
+    for (let x = 0; x <= GAME_WIDTH / 128; x++) {
+      let block = new Assets(
+        src,
+        { width: 128, heidth: 128 },
+        new Vector2D(x * 128, GAME_HEIGH - 128)
+      );
       this.blocks.push(block);
     }
 
@@ -130,8 +188,9 @@ class Player {
   vy: number = 20;
   weigth: number = 0;
   state: States;
-
+  isDead: boolean;
   constructor() {
+    this.isDead = false;
     this.x = 300;
     this.y = 255;
     this.maxSpeed = 100;
@@ -178,10 +237,10 @@ if (game) {
 }
 
 let gameFrame = 0;
-let stagFrames = 6;
+let stagFrames = 7;
 window.onkeydown = (event) => {
   switch (event.key) {
-    case "d": {
+    case "ArrowRight": {
       player.walking = true;
       if (player.state !== States.JUMPSTART && player.state !== States.ROLL) {
         player.state = States.RUN;
@@ -189,7 +248,7 @@ window.onkeydown = (event) => {
       player.scale = 1;
       break;
     }
-    case "a":
+    case "ArrowLeft":
       {
         player.walking = true;
 
@@ -199,14 +258,14 @@ window.onkeydown = (event) => {
         player.scale = -1;
       }
       break;
-    case "w": {
+    case "ArrowUp": {
       if (player.onGround) {
         player.onGround = false;
         player.state = States.JUMPSTART;
       }
       break;
     }
-    case "s":
+    case "ArrowDown":
       {
         player.weigth = 50;
         player.state = States.ROLL;
@@ -223,29 +282,48 @@ window.onkeydown = (event) => {
       );
       img.velocity *= player.scale;
       bullets.push(img);
+      break;
+    }
+    case "r": {
+      if (player.isDead) {
+        player = new Player();
+        bullets = [];
+      }
+      break;
     }
   }
 };
 
 window.onkeyup = (e) => {
   switch (e.key) {
-    case "d": {
+    case "ArrowLeft": {
       player.walking = false;
       player.state = States.IDLE;
       break;
     }
-    case "a": {
+    case "ArrowRight": {
       player.walking = false;
       player.state = States.IDLE;
       break;
     }
-    case "s":
+    case "ArrowDown":
       {
         player.state = States.IDLE;
       }
       break;
   }
 };
+
+function handleEnemyMovement(
+  enemies: Enemy[],
+  game: CanvasRenderingContext2D | null
+) {
+  if (game) {
+    enemies.forEach((enemy) => {
+      enemy.x -= enemy.vy;
+    });
+  }
+}
 
 function handleMovement(player: Player, game: CanvasRenderingContext2D | null) {
   if (game) {
@@ -261,6 +339,49 @@ function handleMovement(player: Player, game: CanvasRenderingContext2D | null) {
       player.y -= player.vy;
     }
   }
+}
+
+function handleEnemyAnimation(enemies: Enemy[]) {
+  enemies.forEach((enemy,index) => {
+     
+    if (game && !enemy.forRemoval) {
+      let x: number;
+      if (enemy.scale < 0) {
+        x = -(enemy.x + enemy.width);
+      } else {
+        x = enemy.x;
+      }
+
+      switch (enemy.state) {
+        case States.RUN: {
+          drawEnemy(enemy, States.RUN, game, x);
+          break;
+        }
+
+        case States.HIT: {
+          drawEnemy(enemy, States.HIT, game, x);
+          break;
+        }
+        case States.JUMPEND: {
+          drawEnemy(enemy, States.JUMPEND, game, x);
+          player.state = States.IDLE;
+          break;
+        }
+        case States.IDLE: {
+          drawEnemy(enemy, States.IDLE, game, x);
+          break;
+        }
+        case States.DEATH: {
+          drawEnemy(enemy,States.DEATH,game,x);
+          break;
+        }
+      }
+    }
+    else {
+      enemies.splice(index,1);
+      console.log(enemies);
+    }
+  });
 }
 
 function handleAnimation() {
@@ -305,14 +426,28 @@ function handleAnimation() {
 
 function drawBullets() {
   bullets.forEach((bullet, index) => {
-    if (bullet.end >= 550) {
+    if (bullet.end >= GAME_WIDTH / 2) {
       bullets.splice(index, 1);
     } else {
-      if(bullet.isFirstShot){
-        let firshot = new Assets('assets/muzzle.png',{width:128,heidth:128},bullet.position);
-        game?.save()
-        game?.scale(player.scale,1);
-        game?.drawImage(firshot.asset,0,0,bullet.size.width,bullet.size.heidth,(bullet.position.x+12)*player.scale,bullet.position.y - 17,64,64);
+      if (bullet.isFirstShot) {
+        let firshot = new Assets(
+          "assets/muzzle.png",
+          { width: 128, heidth: 128 },
+          bullet.position
+        );
+        game?.save();
+        game?.scale(player.scale, 1);
+        game?.drawImage(
+          firshot.asset,
+          0,
+          0,
+          bullet.size.width,
+          bullet.size.heidth,
+          (bullet.position.x + 12) * player.scale,
+          bullet.position.y - 17,
+          64,
+          64
+        );
         bullet.isFirstShot = false;
         game?.restore();
       }
@@ -333,6 +468,56 @@ function drawBullets() {
   });
 }
 
+for(let x = 0; x<= 10; x++){
+enemies.push(new Enemy());
+}
+
+function drawEnemy(
+  enemy: Enemy,
+  state: States,
+  game: CanvasRenderingContext2D,
+  x: number
+) {
+  game.save();
+  game.scale(enemy.scale, 1);
+  if (!(enemy.healtBar.width < 0)) {
+        game.fillStyle = "green";
+    game.fillRect(x, enemy.y - 20, enemy.healtBar.width, enemy.healtBar.heidth);
+  }
+
+  let idleAnim = enemy.animations.get(state);
+  if (idleAnim) {
+    let pos = Math.floor((gameFrame / stagFrames) % idleAnim.frames);
+    if (idleAnim.images[pos]) {
+      game.drawImage(
+        idleAnim.images[pos],
+        720,
+        980,
+        650,
+        750,
+        x,
+        enemy.y,
+        enemy.width,
+        enemy.height
+      );
+      game.restore();
+      idleAnim.currentFrame++;
+      if (idleAnim.currentFrame > idleAnim.frames) {
+        if(enemy.state === States.DEATH) {
+          enemy.forRemoval = true;
+        }
+        else if (
+          idleAnim.currentFrame > idleAnim.frames &&
+          enemy.state === States.HIT
+        ) {
+          enemy.state = States.RUN;
+        }
+
+        idleAnim.currentFrame = 0;
+      }
+    }
+  }
+}
 function drawAnimation(
   state: States,
   game: CanvasRenderingContext2D,
@@ -357,7 +542,6 @@ function drawAnimation(
         player.height
       );
       game.restore();
-      gameFrame++;
       idleAnim.currentFrame++;
       if (idleAnim.currentFrame > idleAnim.frames) idleAnim.currentFrame = 0;
     }
@@ -368,7 +552,7 @@ var level = new Level("assets/spaceTile2.png");
 function loadLevel(game: CanvasRenderingContext2D | null) {
   if (game) {
     game.drawImage(level.background_image.img, 0, 0);
-    for (let x = 0; x < 10; x++) {
+    for (let x = 0; x < GAME_WIDTH / 128; x++) {
       let image = level.blocks[x];
       if (image) {
         game.beginPath();
@@ -379,18 +563,71 @@ function loadLevel(game: CanvasRenderingContext2D | null) {
   }
 }
 
-function applyGravity(player: Player) {
+function applyPlayerGravity(player: Player) {
   if (player.weigth > player.vy && player.state !== States.ROLL)
     player.state = States.FALL;
   player.y += player.weigth;
   player.weigth = player.weigth + 1;
 }
 
+function applyEnemyGravity(enemies: Enemy[]) {
+  enemies.forEach((enemy) => {
+    enemy.y += enemy.weigth;
+    enemy.weigth += 1;
+  });
+}
+
+function handleCollisionEnemy(enemy: Enemy, level: Level) {
+  var collision: boolean = false;
+  for (let x = 0; x < level.blocks.length; x++) {
+    var image = level.blocks[x];
+    if (
+      image &&
+      enemy.y + enemy.height >= image.position.y &&
+      enemy.x + enemy.width <= image.position.x + image.size.width / 2
+    ) {
+      enemy.onGround = true;
+      enemy.weigth = 0;
+      collision = true;
+      enemy.y = image.position.y - enemy.height;
+    }
+  }
+  for (let x = 0; x < bullets.length; x++) {
+    let bullet = bullets[x];
+    if (bullet) {
+      if (
+        bullet.position.x >= enemy.x && bullet.position.x <=enemy.x + enemy.width &&
+        bullet.position.y <= enemy.y + enemy.width
+        || bullet.position.x <= enemy.x+enemy.width && bullet.position.x >= enemy.x ) {
+        bullets.splice(x, 1);
+        enemy.state = States.HIT;
+        enemy.healtBar.width -= 5;
+        if(enemy.healtBar.width<= 0 ) enemy.state = States.DEATH;
+      }
+  }
+  }
+  if (enemy.x + enemy.width >= GAME_WIDTH) {
+    enemy.x = GAME_WIDTH - enemy.width;
+  }
+  if (enemy.y + enemy.height >= GAME_HEIGH) {
+    enemy.isDead = true;
+  }
+  if (enemy.x <= 0) {
+    enemy.x = 0;
+  }
+
+  return collision;
+}
+
 function handleCollision(player: Player, level: Level) {
   var collision: boolean = false;
   for (let x = 0; x < level.blocks.length; x++) {
     var image = level.blocks[x];
-    if (image && player.y + player.height >= image.position.y && player.x + player.width <= image.position.x + image.size.width/2) {
+    if (
+      image &&
+      player.y + player.height >= image.position.y &&
+      player.x + player.width <= image.position.x + image.size.width / 2
+    ) {
       player.onGround = true;
       player.weigth = 0;
       collision = true;
@@ -400,6 +637,9 @@ function handleCollision(player: Player, level: Level) {
   }
   if (player.x + player.width >= GAME_WIDTH) {
     player.x = GAME_WIDTH - player.width;
+  }
+  if (player.y + player.height >= GAME_HEIGH) {
+    player.isDead = true;
   }
   if (player.x <= 0) {
     player.x = 0;
@@ -425,7 +665,7 @@ function drawWeapon() {
 }
 
 let lastTime = 0;
-let fps = 60;
+let fps = 30;
 let interval = 1000 / fps;
 let acumilatedTime = 0;
 
@@ -439,15 +679,29 @@ function mainLoop(timestamp: number) {
 
   this.time = timestamp;
   if (acumilatedTime >= interval) {
-    //let deltaTime = dt / 1000;
-    handleMovement(player, game);
-    applyGravity(player);
-    handleCollision(player, level);
-    loadLevel(game);
-    handleAnimation();
-    drawWeapon();
-    drawBullets();
-    requestAnimationFrame(mainLoop);
+    if (player.isDead && game) {
+      loadLevel(game);
+      game.font = "48px serif";
+      game.fillStyle = "red";
+      game.fillText("DEAD! GAME OVER!", GAME_WIDTH / 2.5, GAME_HEIGH / 2);
+      requestAnimationFrame(mainLoop);
+    } else {
+      handleMovement(player, game);
+      handleEnemyMovement(enemies, game);
+      applyPlayerGravity(player);
+      applyEnemyGravity(enemies);
+      handleCollision(player, level);
+      enemies.forEach((enemy) => {
+        handleCollisionEnemy(enemy, level);
+      });
+      loadLevel(game);
+      handleAnimation();
+      drawWeapon();
+      drawBullets();
+      handleEnemyAnimation(enemies);
+      gameFrame++;
+      requestAnimationFrame(mainLoop);
+    }
   }
 }
 
